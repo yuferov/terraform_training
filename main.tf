c1f45662f7ac4795206e6f2e71923f249cf129cf
Konstantin Yuferov <ksyuferov@gmail.com>
2023-05-04 01:44:19 +0300

    hello-world http web-server

--- main.tf
+++ main.tf
@@ -3,11 +3,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-03aefa83246f44ef2"
-  instance_type = "t2.micro"
+  ami                    = "ami-0ec7f9846da6b0f61"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+	      #!/bin/bash
+	      echo "Hello, World" > index.html
+	      nohup busybox httpd -f -p 8080 &
+	      EOF
 
   tags = {
     Name = "terraform-example"
   }
 }
 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
